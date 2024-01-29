084ca8c2e341cc183f11bc3329a83d8613e29cd3
sacha_ek <sacha-ek@yandex.ru>
2024-01-29 11:09:49 +0500

    Добавил docstrings to tensor_main_page.py

--- pages/tensor_main_page.py
+++ pages/tensor_main_page.py
@@ -4,6 +4,20 @@ from .tensor_about_page import TensorAboutPage
 
 
 class TensorMainPage(BasePage):
+    """
+    Represents the Tensor Main page with specific methods for checking the Power of Man block.
+
+    Attributes:
+        link (str): The URL of the Tensor Main page.
+
+    Methods:
+        checking_the_block_power_of_man(): Checks the Power of Man block, clicks the ABOUT button, and checks for
+        the Tensor About page.
+        should_be_power_of_man_block(): Asserts the presence of the Power of Man block on the page.
+        should_be_button_about(): Asserts the presence of the ABOUT button in the Power of Man block.
+        click_button_about(): Clicks the ABOUT button and scrolls to the element.
+        check_for_tensor_about_page(): Checks if the current page is the Tensor About page.
+        """
     link = 'https://tensor.ru/'
 
     def checking_the_block_power_of_man(self):
@@ -26,6 +40,12 @@ class TensorMainPage(BasePage):
         self.browser.find_element(*TensorMainPageLocators.POWER_OF_MAN_BLOCK_ABOUT_BUTTON).click()
 
     def check_for_tensor_about_page(self):
+        """
+           Checks if the current page is the Tensor About page.
+
+           Raises:
+               AssertionError: If the current page is not the Tensor About page.
+        """
         new_window = self.browser.window_handles[-1]
         self.browser.switch_to.window(new_window)
         assert TensorAboutPage.link == self.browser.current_url, \
